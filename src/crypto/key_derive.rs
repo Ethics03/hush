2783719e162bc2e7dfa4388db20b801e6d36bf3c
Ethics03 <honey032004@gmail.com>
2025-10-19 19:07:38 +0530

    feat: added key derivation

---
+++ src/crypto/key_derive.rs
@@ -0,0 +1,14 @@
+use argon2::password_hash::SaltString;
+use argon2::Argon2;
+
+pub fn derive_key(password: &str, salt: &SaltString) -> Result<[u8; 32], String> {
+    let argon2 = Argon2::default();
+
+    let mut key = [0u8; 32];
+
+    argon2
+        .hash_password_into(password.as_bytes(), salt.as_ref().as_bytes(), &mut key)
+        .map_err(|e| format!("Key derivation failed: {}", e))?;
+
+    Ok(key)
+}
