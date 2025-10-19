d5d5641643dde5d9085a07fbb42cdabacc6be997
Ethics03 <honey032004@gmail.com>
2025-10-19 13:43:34 +0530

    feat: defining commands

---
+++ src/cli.rs
@@ -0,0 +1,45 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "hush")]
+#[command(about = "A secure secret management CLI", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Create {
+        //name of the vault
+        name: Option<String>,
+    },
+
+    Set {
+        #[arg(short, long)]
+        vault: String,
+
+        #[arg(short, long)]
+        key: String,
+
+        #[arg(short = 'V', long)]
+        value: String,
+    },
+
+    //getting the value of the secret_key
+    Get {
+        #[arg(short, long)]
+        vault: String,
+
+        #[arg(short, long)]
+        key: String,
+    },
+
+    Delete {
+        #[arg(short, long)]
+        vault: String,
+
+        #[arg(short, long)]
+        key: String,
+    },
+}
