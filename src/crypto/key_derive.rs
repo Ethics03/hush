00511d0f2c012bd8afa30d9bae6eed1416d078c5
Ethics03 <honey032004@gmail.com>
2025-10-20 19:48:17 +0530

    feat: made key derivation function anyhow vaible

--- src/crypto/key_derive.rs
+++ src/crypto/key_derive.rs
@@ -1,14 +1,13 @@
+use anyhow::Result;
 use argon2::password_hash::SaltString;
 use argon2::Argon2;
-
-pub fn derive_key(password: &str, salt: &SaltString) -> Result<[u8; 32], String> {
+pub fn derive_key(password: &str, salt: &SaltString) -> Result<[u8; 32]> {
     let argon2 = Argon2::default();
 
     let mut key = [0u8; 32];
 
     argon2
         .hash_password_into(password.as_bytes(), salt.as_ref().as_bytes(), &mut key)
-        .map_err(|e| format!("Key derivation failed: {}", e))?;
-
+        .map_err(|e| anyhow::anyhow!("Key derivation failed: {}", e))?;
     Ok(key)
 }
